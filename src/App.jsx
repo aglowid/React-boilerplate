5958c7dd123eb2fe7c07bcf2559afeeac503ff9d
Aglowid <info@aglowiditsolutions.com>
2023-01-04 19:23:26 +0530

    Develop (#11)
    * Feature/setup (#8)
    
    * chore: added new script for installation process in Read me
    
    * Purpose: This PR is for checking workflows after the repo migration. (#2)
    
    * fix: resolving codacy reported css lint errors
    
    * chore: testing workflows
    
    Co-authored-by: kushagra-aglowid <121283324+kushagra-aglowid@users.noreply.github.com>
    
    * chore: testing commit lint action workflow
    
    Co-authored-by: kushagra-aglowid <121283324+kushagra-aglowid@users.noreply.github.com>
    
    * chore: testing commit lint (#10)
    
    Co-authored-by: kushagra-aglowid <121283324+kushagra-aglowid@users.noreply.github.com>
    
    Co-authored-by: kushagra-aglowid <121283324+kushagra-aglowid@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Happy Coding...</p>
+        <p>Happy Coding</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Boilerplate
+          React Boilerplate
         </a>
       </header>
     </div>
